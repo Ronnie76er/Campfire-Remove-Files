61309cbea0cb89e77f0ab0428380bcd9b146e4d1
Ron Alleva <ron.alleva@gmail.com>
2012-01-25 23:45:55 -0500

    Cleaned up the code some and added some prompts for things.

--- campfireremovefiles.rb
+++ campfireremovefiles.rb
@@ -5,61 +5,94 @@ require 'net/https'
 require 'optparse'
 require 'json'
 
-options = {}
-OptionParser.new do |opts|
 
-	opts.banner = "Usage: example.rb [options]"
 
-	opts.on("-s", "--server SERVER", "Campfire server URL (name.campfirenow.com)") do |s|
-		options[:server] = s
+def get_options()
+	options = {}
+	OptionParser.new do |opts|
+
+		opts.banner = "Usage: example.rb [options]"
+
+		opts.on("-s", "--server SERVER", "Campfire server URL (name.campfirenow.com)") do |s|
+			options[:server] = s
+		end
+
+		opts.on("-t", "--token TOKEN", "API Authentication token") do |t|
+			options[:token]= t
+		end
+
+
+	end.parse!
+
+	return options
+end
+
+def choose_room(https, token)
+	req = Net::HTTP::Get.new('/rooms.json')
+	req.basic_auth token, 'x'
+	resp, data = https.request(req)
+
+	value = JSON.parse(data)
+
+	puts "Choose the room you want to delete the files from: "
+	value['rooms'].each_with_index do |room, index|
+		puts "#{index}: #{room['name']}"
 	end
+	puts "#{value['rooms'].length}: All"
+	room_index = gets.chomp.to_i
 
-	opts.on("-t", "--token TOKEN", "API Authentication token") do |t|
-		options[:token]= t
+	if room_index == value['rooms'].length
+		puts "This will delete all files in all rooms!  Are you sure? (y/n)"
+		if gets.chomp != 'y'
+			exit
+		end
+		return value['rooms'].collect{|room| room['id']}
+	elsif room_index > value['rooms'].length or room_index < 0
+		raise "Invalid Room choice"
+	else
+		puts "This will delete all files in the room #{value['rooms'][room_index]['name']}!!  Are you sure? (y/n)"
+		if gets.chomp != 'y'
+			exit
+		end
+
+		return [value['rooms'][room_index]['id']]
 	end
+end
 
 
-end.parse!
+
+options = get_options()
 
 https = Net::HTTP.new(options[:server], 443)
 https.use_ssl = true
 begin
 	https.start
 
+	room_ids = choose_room(https, options[:token])
 
-
-	req = Net::HTTP::Get.new('/rooms.json')
-	req.basic_auth options[:token], 'x'
-	resp, data = https.request(req)
-
-	value = JSON.parse(data)
-	p value["rooms"][0]["id"]
-
-
-	while true
-		request_path = "/room/#{value['rooms'][0]['id']}/uploads.json"
-		puts request_path
-		req = Net::HTTP::Get.new(request_path)
-		req.basic_auth options[:token], 'x'
-		resp, data = https.request(req)
-
-		files = JSON.parse(data)
-
-		uploads = files["uploads"]
-
-		break if uploads.length == 0
-
-		uploads.each do |upload|
-			puts "#{upload["name"]} : #{upload["id"]}"
-			request_path = "/upload/delete/#{upload['id']}"
-			puts request_path
-			req = Net::HTTP::Post.new(request_path)
+	room_ids.each do |room_id|
+		while true
+			request_path = "/room/#{room_id}/uploads.json"
+			req = Net::HTTP::Get.new(request_path)
 			req.basic_auth options[:token], 'x'
 			resp, data = https.request(req)
-			p resp
+
+			files = JSON.parse(data)
+
+			uploads = files["uploads"]
+
+			break if uploads.length == 0
+
+			uploads.each do |upload|
+				puts "Deleting #{upload["name"]}"
+				request_path = "/upload/delete/#{upload['id']}"
+				req = Net::HTTP::Post.new(request_path)
+				req.basic_auth options[:token], 'x'
+				resp, data = https.request(req)
+			end
 		end
 	end
-	https.finish
+
 rescue SocketError => e
 	puts "Cannot connect to server #{options[:server]}"
 rescue => e
